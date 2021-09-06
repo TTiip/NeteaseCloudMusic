92accaf5b3676d66466434d2e94658313eeea11d
TTiip <2711430287@qq.com>
2021-09-06 22:41:19 +0800

    ...

--- vue.config1.js
+++ vue.config1.js
@@ -1,14 +1,20 @@
 const path = require('path')
 
-module.exports = {
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'less',
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
       patterns: [
         path.resolve(__dirname, './assets/less/common.less'),
         path.resolve(__dirname, './assets/less/reset.less')
       ]
-    }
+    })
+}
+
+module.exports = {
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)))
   },
   configureWebpack: {
     resolve: {
