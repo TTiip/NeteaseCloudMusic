e3b93917a3a9eb1ec9c9af1cc7d0f72c27c5ebdb
TTiip <2711430287@qq.com>
2021-09-21 23:37:46 +0800

    导航歌单页面获取数据重复问题修改

--- src/components/play-list-page/play-list-page.vue
+++ src/components/play-list-page/play-list-page.vue
@@ -33,17 +33,6 @@ const props = defineProps({
 })
 
 /* data */
-const svg = `
-  <path class="path" d="
-    M 30 15
-    L 28 17
-    M 25.61 25.61
-    A 15 15, 0, 0, 1, 15 30
-    A 15 15, 0, 1, 1, 27.99 7.5
-    L 15 15
-  " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
-`
-
 const list: any = ref([])
 const isLoading: any = ref(false)
 
@@ -65,7 +54,8 @@ const getPlayList = async (params: any) => {
   isLoading.value = true
   const getTopPlayListData = await axios({
     url: 'getTopPlayList',
-    method: 'GET'
+    method: 'GET',
+    params
   })
 
   list.value = params.offset !== 0 ? [...list.value, ...getTopPlayListData.playlists] : getTopPlayListData.playlists
