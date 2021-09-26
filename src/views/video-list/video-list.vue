9c21da892968ab0af0db2e9407aadd53a7951301
TTiip <2711430287@qq.com>
2021-09-26 16:11:53 +0800

    添加搜索列表video列表展示页

---
+++ src/views/video-list/video-list.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="video">
+    <div
+      v-for="(item, index) in videoList"
+      :key="'' + item.id + Number(index)"
+      class="item"
+    >
+      <router-link
+        :to="{ path: item.type ? '/video' : '/mv', query: { id: item.vid }}"
+        class="faceImg"
+      >
+        <i class="iconfont icon-play" />
+        <el-image :src="item.coverUrl">
+          <template #placeholder>
+            <div class="image-slot">
+              <i class="iconfont icon-placeholder" />
+            </div>
+          </template>
+        </el-image>
+        <span class="playCount"><i class="iconfont icon-video" /><em>{{ $utils.formartNum(item.playTime) }}</em></span>
+        <div class="v-time">
+          {{ utils.formatSongTime(item.durationms) }}
+        </div>
+      </router-link>
+      <div class="info">
+        <router-link
+          :to="{ path: '/video', query: { id: item.vid }}"
+          class="v-name"
+        >
+          {{ item.title }}
+        </router-link>
+        <router-link
+          v-for="(author, k) in item.creator"
+          :key="k"
+          :to="{ path: '/user', query: { uid: author.userId }}"
+          class="v-author"
+        >
+          By. {{ k !== 0 ? ' / ' + author.userName : author.userName }}
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import utils from '@/utils'
+defineProps({
+  videoList: {
+    type: Array as any,
+    default: () => ([])
+  }
+})
+</script>
+
+<style scoped lang="less">
+@import './index.less';
+</style>
